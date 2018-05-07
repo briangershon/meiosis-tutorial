57725a07abf37bc781c57da4940c9cfa1e13119f
Brian Gershon <briang@unity3d.com>
2018-05-07 16:52:15 -0700

    Replace updateModel() with stream scan(). Support “Add” and “times”. Still pass a second view param to support label text.

--- app.jsx
+++ app.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-use-before-define, func-names */
 /* global React, ReactDOM */
 
-function stream() {
+function stream(initial) {
   const mapFunctions = [];
   function createdStream(value) {
     mapFunctions.forEach((fn) => {
@@ -9,7 +9,12 @@ function stream() {
     });
   }
   createdStream.map = function (mapFunction) {
-    const newStream = stream();
+    let newInitial;
+    if (initial !== undefined) {
+      // call map with initial value
+      newInitial = mapFunction(initial);
+    }
+    const newStream = stream(newInitial);
 
     mapFunctions.push((value) => {
       newStream(mapFunction(value));
@@ -20,19 +25,38 @@ function stream() {
   return createdStream;
 }
 
+function scan(accumulator, initial, sourceStream) {
+  const newStream = stream(initial);
+  let accumulated = initial;
+
+  sourceStream.map((value) => {
+    accumulated = accumulator(accumulated, value);
+    newStream(accumulated);
+    return null;
+  });
+
+  return newStream;
+}
+
 function createView(updateFn) {
   function increase(amount) {
     return function () {
-      updateFn(amount);
+      updateFn({ oper: 'add', value: amount });
     };
   }
 
-  function view(m) {
+  function times(amount) {
+    return function () {
+      updateFn({ oper: 'times', value: amount });
+    };
+  }
+
+  function view(m, label) {
     return (
       <div>
-        <div>{m.label} is at {m.value}.</div>
+        <div>{label} is at {m}.</div>
         <button onClick={increase(1)}>+1</button>
-        <button onClick={increase(-1)}>-1</button>
+        <button onClick={times(2)}>*2</button>
         <button onClick={increase(5)}>+5</button>
         <button onClick={increase(-5)}>-5</button>
       </div>);
@@ -40,25 +64,25 @@ function createView(updateFn) {
   return view;
 }
 
-const model = { label: 'The Counter', value: 0 };
-
 const update = stream();
 const view = createView(update);
 
+const models = scan((model, obj) => {
+  if (obj.oper === 'add') {
+    return model + obj.value;
+  }
+  if (obj.oper === 'times') {
+    return model * obj.value;
+  }
+  return 9999999;
+}, 0, update);
+
 const element = document.getElementById('app');
 
-function updateModel(value) {
-  model.value += value;
-  return model;
+function render(viewFromModel) {
+  ReactDOM.render(viewFromModel, element);
 }
 
-function render(value) {
-  ReactDOM.render(value, element);
-}
-
-update
-  .map(updateModel)
-  .map(view)
+models
+  .map(value => view(value, 'The Counter'))
   .map(render);
-
-ReactDOM.render(view(model), element);
