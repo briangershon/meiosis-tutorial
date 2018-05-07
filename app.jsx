8012d11ecfe9063257adbe2f0fd61048520ef4dd
Brian Gershon <briang@unity3d.com>
2018-05-07 14:43:54 -0700

    Add createView()

--- app.jsx
+++ app.jsx
@@ -1,27 +1,34 @@
 /* eslint-disable no-use-before-define, func-names */
 /* global React, ReactDOM */
 
-const element = document.getElementById('app');
-const model = { label: 'The Counter', value: 0 };
+function createView(updateFn) {
+  function increase(amount) {
+    return function () {
+      updateFn(amount);
+    };
+  }
 
-function view(m) {
-  return (
-    <div>
-      <div>{m.label} is at {m.value}.</div>
-      <button onClick={increase(1)}>+1</button>
-      <button onClick={increase(-1)}>-1</button>
-    </div>);
+  function view(m) {
+    return (
+      <div>
+        <div>{m.label} is at {m.value}.</div>
+        <button onClick={increase(1)}>+1</button>
+        <button onClick={increase(-1)}>-1</button>
+        <button onClick={increase(5)}>+5</button>
+        <button onClick={increase(-5)}>-5</button>
+      </div>);
+  }
+  return view;
 }
+
+const model = { label: 'The Counter', value: 0 };
+const element = document.getElementById('app');
+let view;
 
 function update(value) {
   model.value += value;
   ReactDOM.render(view(model), element);
 }
 
-function increase(amount) {
-  return function () {
-    update(amount);
-  };
-}
-
+view = createView(update);
 ReactDOM.render(view(model), element);
